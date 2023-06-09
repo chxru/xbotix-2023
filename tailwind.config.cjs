0ad48c06a174943708c01959a7ad6c00bd72bb3c
Charuka Samarakoon <charuka@protonmail.com>
2023-06-09 11:53:18 +0530

    Add navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "10rem",
+        lg: "15rem",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#18181c",
+      },
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
